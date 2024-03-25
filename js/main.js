32de8aa4d28e6aa640c051996ada45cba10670ae
alwry <144924749+alwry@users.noreply.github.com>
2024-03-25 02:10:35 -0400

    Audio functionality
    Fixed bugs
    Added audio functionality

--- js/main.js
+++ js/main.js
@@ -3,13 +3,15 @@ console.log('JS file connected');
 const audioPieces = document.querySelectorAll('.icon-hov img'),
     dropZones = document.querySelectorAll('.dropzone'),
     resetButton = document.querySelector('#reset'),
-    audioBoard = document.querySelectorAll('.icon-hov');
+    audioBoard = document.querySelectorAll('.icon-hov'),
+    theAudioEl = document.querySelector("#audioEl"),
+    spinCd = document.querySelector('#cdspin');
+
+let audioElements = [];
 
 
-
-
-function allowDrop(event) {
-    event.preventDefault();
+function allowDrop(e) {
+    e.preventDefault();
 }
 
 function drag(e) {
@@ -20,20 +22,41 @@ function drag(e) {
 function drop(e) {
     e.preventDefault();
     let itemId = e.dataTransfer.getData('id'),
-    dropZone = e.target;
-    
-
-    if (dropZone.children.length == 0) {
-    e.target.appendChild(document.getElementById(itemId));
-    console.log('dropped something on me');}
+    dropZone = e.currentTarget;
+    if (dropZone.children.length > 0) {
+        console.log('no');}
     else {
-        console.log('please dont');
-    }
+        e.target.appendChild(document.getElementById(itemId));
+        console.log('dropped something on me');
+        let newSrc = `audio/${e.target.querySelector('img').dataset.trackref}.mp3`;
+        let audio = new Audio(newSrc);
+        audioElements.push(audio);
+        audio.load();
+        return (audio);
+    }   
 }
 
 function resetMixer() {
     audioPieces.forEach((piece, index) => {
         audioBoard[index].appendChild(piece);
+    });
+    console.log('reset');
+    resetAudio();
+}
+
+function playAudio(audio){
+    audio.play();
+    audio.loop = true;
+    spinCd.classList.add('spinningcd');
+    audio.addEventListener('pause', function() {
+        spinCd.classList.remove('spinningcd');
+    });
+}
+
+function resetAudio(){
+    audioElements.forEach(audio => {
+        audio.pause();
+        audio.currentTime = 0;
     });
 }
 
@@ -41,20 +64,11 @@ function resetMixer() {
 audioPieces.forEach(piece => {
     piece.addEventListener('dragstart', drag);
 });
-
 dropZones.forEach(zone => {
     zone.addEventListener('dragover', allowDrop);
-    zone.addEventListener('drop', drop);
+    zone.addEventListener('drop', (e) => {
+        const audio = drop(e);
+        playAudio(audio);
+    });
 });
-
 resetButton.addEventListener("click", resetMixer);
-// console.log('JS file connected')
-
-// const audioPieces = document.querySelectorAll('.icon-hov img'),
-//     dropZones = document.querySelectorAll('.dropzone'),
-//     audioBoard = document.querySelector('.grid-small');
-
-// // let resetButton = document.querySelector('#reset'),
-// //     playButton = document.querySelector('#play'),
-// //     draggedAudio;
-    
